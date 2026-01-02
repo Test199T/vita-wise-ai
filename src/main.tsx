6ace85a8a7e69a9bb595558bf378df984dfd91f7
Mac papon <asngiun@gmail.com>
2026-01-02 11:59:05 +0700

    feat: add console branding and security warning for production

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Console Branding & Security Warning for Production
+import './utils/console-branding'
+
 createRoot(document.getElementById("root")!).render(<App />);
